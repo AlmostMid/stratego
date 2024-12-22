18ce274231b1652e533ca2e884817a6c92de9788
victor mazanti <victormazanti@hotmail.com>
2024-12-22 10:18:30 +0100

    Frame set up blank and running in main

---
+++ UI.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class UI {
+
+    public void Run() {
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("Stratego");
+        frame.setVisible(true);
+
+    }
+    
+}
