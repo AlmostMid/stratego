dd7fd40d1691f2004690c4a068a6d399059bbc45
victor mazanti <victormazanti@hotmail.com>
2024-12-22 11:06:08 +0100

    Startscreen created

--- UI.java
+++ UI.java
@@ -2,20 +2,23 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 
 public class UI {
 
     public void Run() {
         JFrame frame = new JFrame();
-        JLabel startScreenLabel = new JLabel(new ImageIcon("Stratego_pixel_art_screen.png"));
-        startScreenLabel.setSize(1000, 1000);
+        JButton button = new JButton("Start Game");
+        button.setBounds(50, 100, 95, 30);
+        button.setVisible(true);
+        button.setLocation(450, 450);
         frame.setSize(1000, 1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setTitle("Stratego");
         frame.setVisible(true);
-        frame.add(startScreenLabel);
+        frame.add(button);
 
         
 
